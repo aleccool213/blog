2c140782e341f75fb6b25f39bb77f672921ad6de
Alec Brunelle <alec@alec.coffee>
2017-10-21 19:19:46 -0400

    init

---
+++ src/components/Bio.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <p
+        style={{
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Alec Brunelle`}
+          style={{
+            float: 'left',
+            marginRight: rhythm(1 / 4),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+            borderRadius: '50%'
+          }}
+        />
+        Written by <strong>Alec Brunelle</strong> who lives and works in Toronto building useful things.{' '}
+        <a href="https://twitter.com/yourboybigal">
+          Follow me on Twitter
+        </a>
+      </p>
+    )
+  }
+}
+
+export default Bio
