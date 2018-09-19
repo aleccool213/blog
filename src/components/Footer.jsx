969a4eae1414123e54958cfec443b4f14333657a
Alec Brunelle <alec@alec.coffee>
2018-09-18 20:13:14 -0400

    better footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,14 @@ export const Footer = () => {
     <span>
       <h2>
         ❤️ If you found this article useful, consider{' '}
-        <a href="http://eepurl.com/dfs9Db">subbing</a> for more in the future
-        and <a>tweeting</a> it out. ❤️
+        <a target="_blank" href="http://eepurl.com/dfs9Db">
+          subbing
+        </a>{' '}
+        for more in the future and{' '}
+        <a target="_blank" href="https://twitter.com">
+          tweeting
+        </a>{' '}
+        it out. ❤️
       </h2>
       <blockquote>
         Want to comment? Head over to the Medium version of this post to do so.
