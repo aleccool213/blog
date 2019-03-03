49b7cac5c16594222920c17a02a1cf7d63156756
aleccool213 <alec@alec.coffee>
2019-03-03 08:43:30 -0500

    trying

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
-import "./index.css"
 
 class BlogIndex extends React.Component {
   render() {
