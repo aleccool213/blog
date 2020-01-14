3a35f0de9bb8aa75359a548cc15871c69d81d358
aleccool213 <alec@alec.coffee>
2020-01-14 10:09:12 -0500

    wip

---
+++ content/posts/2018-08-20-elixir-better-programmer/images/patterns.ex
@@ -0,0 +1,5 @@
+defmodule Fib do 
+  def fib(0), do: 0
+  def fib(1), do: 1
+  def fib(n), do: fib(n-1) + fib(n-2)
+end
---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+/**
+ * Paste in your SVG logo and return it from this component.
+ * Make sure you have a height set for your logo.
+ * It is recommended to keep the height within 25-35px.
+ */
+export default function Logo() {
+  return (
+    <svg viewBox="0 0 106 28" height="30px">
+      <path d="M62.9 12h2.8v10...." />
+      <path fill="#fff" d="M25 14h-..." />
+      <path d="M14 0C6.3..." fill="#639" />
+    </svg>
+  );
+}
