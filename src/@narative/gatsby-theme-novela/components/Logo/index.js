51a5a8141f77435989c7444a70de017d32f95446
aleccool213 <alec@alec.coffee>
2020-01-16 09:46:20 -0500

    Get logo/title/author looking nice

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 
+import svgLogo from "./coffee.svg";
+
 /**
  * Paste in your SVG logo and return it from this component.
  * Make sure you have a height set for your logo.
  * It is recommended to keep the height within 25-35px.
  */
 export default function Logo() {
-  return (
-    <svg viewBox="0 0 106 28" height="30px">
-      <path d="M62.9 12h2.8v10...." />
-      <path fill="#fff" d="M25 14h-..." />
-      <path d="M14 0C6.3..." fill="#639" />
-    </svg>
-  );
+  return <img src={svgLogo} width="100px" height="100px" />;
 }
